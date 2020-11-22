bf6a7c9080697267cd5920ce0a24834d0c01bac6
juanlikesMusic <67354004+juanlikesMusic@users.noreply.github.com>
2020-11-21 23:01:23 -0600

    Create mi-footer.js

---
+++ cmp/mi-footer.js
@@ -0,0 +1,7 @@
+class MiFooter extends HTMLElement {
+  connectedCallback(){
+    this.innerHTML = */ html*/
+      'Copyright &copy; Alvarez Hernández Juan Pablo';
+  }
+}
+ customElements.define("mi-footer", MiFooter);    
